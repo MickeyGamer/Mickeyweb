901ab484828e01f6fc1c7a7d14213a39e8c5f2a1
MickeyGamer <gmastercasgamer@gmail.com>
2026-06-04 10:22:00 +0800

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+        ยินดีต้อนรับเข้าสู่ Website ของผมครับ
+        <p>โดย ธนกรณ์ รัศมีจันทร์</p>
+    </div>
+  );
+}

4f29917f36752fdc74bfa196cba1788398450cca
MickeyGamer <gmastercasgamer@gmail.com>
2026-06-04 11:23:18 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <div>
-        ยินดีต้อนรับเข้าสู่ Website ของผมครับ
+        <Navbar />
+        <h2>ยินดีต้อนรับเข้าสู่ Website ของผมครับ</h2>
         <p>โดย ธนกรณ์ รัศมีจันทร์</p>
     </div>
   );

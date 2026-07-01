c91de39586fce8105b9fe2afddee5cbf94acb41c
MickeyGamer <gmastercasgamer@gmail.com>
2026-07-02 00:15:11 +0800

    update

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,21 +1,24 @@
-import { redirect } from "next/navigation";
+import { redirect } from "next/navigation"
 import { getSession } from "@/lib/session";
 
 export default async function Dashboard() {
 
-  const user = await getSession();
+  const user = await getSession ();
 
   if (!user) {
     redirect("/login");
   }
 
+
   return (
     <div>
 
-      <h2> Dashboard </h2>
-      <p>welcome {user.name as string}</p>
-      <p>Role: {user.role as string}</p>
-
+    <h2> Dashboard</h2>
+    <p>Wellcome: {user.name as string} </p>
+    <p>Role: {user.name as string} </p>
+    <p>School: {user.school as string} </p>
     </div>
+
   );
+
 }

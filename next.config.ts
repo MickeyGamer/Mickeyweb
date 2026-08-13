790c4fe3b92dc8648929c373c3f11dd3f9a02b1e
MickeyGamer <gmastercasgamer@gmail.com>
2026-08-13 10:21:29 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,16 @@
 import type { NextConfig } from "next";
 
-const devOrigins = process.env.NODE_ENV === 'development' 
-  ? ['localhost', '127.0.0.1'] 
-  : [];
-
 const nextConfig: NextConfig = {
-  allowedDevOrigins: devOrigins,
-  
-  experimental: {
+  /* config options here */
+    images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+    ],
   },
-};
 
+};
 export default nextConfig;
